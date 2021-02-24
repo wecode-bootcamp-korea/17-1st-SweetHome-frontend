f42c330a18b6aa39322154da7089a69e668cb78e
playck <playck@github.com>
2021-02-24 22:23:28 +0900

    Add: 선택 상품 장바구니 백엔드에 담기

--- src/Pages/ProductDetail/ProductInfo/ProductInfo.js
+++ src/Pages/ProductDetail/ProductInfo/ProductInfo.js
@@ -8,12 +8,22 @@ class ProductDetail extends Component {
       itemImgList: [],
       imgNo: 0,
       selectdItem: [],
+      size: "",
+      color: "",
+      quantity: 1,
     };
   }
 
   handleImgChange = (id) => {
     this.setState({
       imgNo: id,
+    });
+  };
+
+  onhandleChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value,
     });
   };
 
@@ -29,12 +39,29 @@ class ProductDetail extends Component {
       });
   };
 
+  onSendOrderInfo = () => {
+    const { size, color, quantity } = this.state;
+    fetch(`http://10.58.2.37:8000/products/${this.props.root}`, {
+      method: "POST",
+      body: JSON.stringify({
+        size: size,
+        color: color,
+        quantity: quantity,
+      }),
+    })
+      .then((response) => response.json())
+      .then((result) => console.log("결과: ", result));
+  };
+
   componentDidMount() {
     this.onRequestProductDetail();
   }
 
   render() {
-    const { selectdItem, imgNo } = this.state;
+    const { selectdItem, imgNo, size, color, quantity } = this.state;
+    console.log(color);
+    console.log(size);
+    console.log(quantity);
     return (
       <div className="productDetail">
         <div className="itmeImgOverview">
@@ -116,31 +143,64 @@ class ProductDetail extends Component {
                 </div>
                 <div className="optionInputBox">
                   {selectdItem.product.size && (
-                    <select type="select option" className="formControl">
+                    <select
+                      type="select option"
+                      className="formControl"
+                      onChange={this.onhandleChange}
+                    >
                       <option disabled>사이즈</option>
                       {selectdItem.product.size.map((size, idx) => {
-                        return <option value={idx}>{size}</option>;
+                        return (
+                          <option key={idx} value={size} name="size">
+                            {size}
+                          </option>
+                        );
                       })}
                     </select>
                   )}
                   {selectdItem.product.color && (
-                    <select type="select option" className="formControl">
+                    <select
+                      type="select option"
+                      className="formControl"
+                      onChange={this.onhandleChange}
+                    >
                       <option disabled>색상</option>
                       {selectdItem.product.color.map((color, idx) => {
-                        return <option value={idx}>{color}</option>;
+                        return (
+                          <option key={idx} value={color} name="color">
+                            {color}
+                          </option>
+                        );
                       })}
                     </select>
                   )}
+                  <div className="orderInfoBox">
+                    <div>{selectdItem.product.name}</div>
+                    <div>{size}</div>
+                    <div>{color}</div>
+                    <select type="select option">
+                      <option>1</option>
+                      <option>2</option>
+                      <option>3</option>
+                      <option>4</option>
+                      <option>5</option>
+                    </select>
+                  </div>
                   <div className="optionPriceBox">
                     <div className="priceBoxLeft">
                       <span>주문금액</span>
                     </div>
                     <div className="priceBoxRight">
-                      <span>0</span>원
+                      <span>
+                        {selectdItem.product.discount_price * quantity}
+                      </span>
+                      원
                     </div>
                   </div>
                   <div className="itemBuyOption">
-                    <button className="cart">장바구니</button>
+                    <button className="cart" onClick={this.onSendOrderInfo}>
+                      장바구니
+                    </button>
                     <button className="buy">바로구매</button>
                   </div>
                 </div>
