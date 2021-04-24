66ffd04875daaf0354e7b8d029a25547598dd898
playck <playck@naver.com>
2021-04-24 21:29:20 +0900

    Modify: MainCategory 함수형 컴포넌트 Refactoring

--- src/Pages/ProductMain/ProductCategory/MainCategory/MainCategory.js
+++ src/Pages/ProductMain/ProductCategory/MainCategory/MainCategory.js
@@ -1,53 +1,47 @@
-import React, { Component } from "react";
+import React from "react";
 import "./MainCategory.scss";
 
-class MainCategory extends Component {
-  render() {
-    const {
-      selectedCategory,
-      categoryListOnOff,
-      onSelectCategory,
-      isListOnOff,
-    } = this.props;
-    return (
-      <div className="mainCategoryItem">
-        <div>
-          {selectedCategory &&
-            selectedCategory.map((category) => {
-              return (
-                <h2
-                  className="mainCategoryItemName"
-                  onClick={categoryListOnOff}
-                >
-                  {category.name}
-                </h2>
-              );
-            })}
-        </div>
-        <ul className="productList">
-          {selectedCategory &&
-            selectedCategory.map((product) => {
-              return (
-                <div>
-                  {isListOnOff && (
-                    <ul className="subProductCategory">
-                      {product.sub_category.map((subProduct) => {
-                        return (
-                          <li onClick={onSelectCategory}>
-                            {subProduct.name}
-                            <button>∨</button>
-                          </li>
-                        );
-                      })}
-                    </ul>
-                  )}
-                </div>
-              );
-            })}
-        </ul>
+const MainCategory = ({
+  selectedCategory,
+  categoryListOnOff,
+  onSelectCategory,
+  isListOnOff,
+}) => {
+  return (
+    <div className="mainCategoryItem">
+      <div>
+        {selectedCategory &&
+          selectedCategory.map((category) => {
+            return (
+              <h2 className="mainCategoryItemName" onClick={categoryListOnOff}>
+                {category.name}
+              </h2>
+            );
+          })}
       </div>
-    );
-  }
-}
+      <ul className="productList">
+        {selectedCategory &&
+          selectedCategory.map((product) => {
+            return (
+              <div>
+                {isListOnOff && (
+                  <ul className="subProductCategory">
+                    {product.sub_category.map((subProduct) => {
+                      return (
+                        <li onClick={onSelectCategory}>
+                          {subProduct.name}
+                          <button>∨</button>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                )}
+              </div>
+            );
+          })}
+      </ul>
+    </div>
+  );
+};
 
 export default MainCategory;
